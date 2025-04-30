658848e1d0a849f12fede6eb1132af5c1a1aee36
Ivan Jovanovic <joi1bg@bosch.com>
2025-04-30 15:34:54 +0200

    First changes

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Invoices Page</h1>
+      <p className="mt-4 text-lg">Welcome to the Invoices Page!</p>
+    </div>
+  );
+}
